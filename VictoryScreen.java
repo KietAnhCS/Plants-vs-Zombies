189c2a4a8296aa0d6e3b7831b4d8a4e8dc48c43f
KietAnhCS <leanhk75@gmail.com>
2026-05-14 09:54:12 +0700

    commit

--- VictoryScreen.java
+++ VictoryScreen.java
@@ -4,17 +4,15 @@ public class VictoryScreen extends World {
     public VictoryScreen() {  
         super(1111, 698, 1); 
         
-        // Tạo nền đen trước
         GreenfootImage bg = new GreenfootImage(1111, 698);
         bg.setColor(Color.BLACK);
         bg.fill();
         setBackground(bg);
         
-        // Phát nhạc (Đảm bảo file tồn tại)
         try {
             AudioManager.getInstance().playSound(80, false, "victory.mp3");
         } catch (Exception e) {
-            System.out.println("Không tìm thấy file nhạc thắng cuộc");
+            System.out.println("Error: not found victory.mp3");
         }
         
         prepare();
@@ -28,7 +26,6 @@ public class VictoryScreen extends World {
         private GifImage gif = new GifImage("victorygif.gif");
 
         public void act() {
-            // Đơn giản nhất là setImage liên tục để GIF chạy
             setImage(gif.getCurrentImage());
         }
     }
